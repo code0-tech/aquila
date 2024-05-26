50c6703d50ab1c0d7c3db740a43b0aae69e344ab
Raphael <raphael.goetz.7@gmail.com>
2024-05-26 11:32:06 +0200

    feat: added rabbitmq connector

---
+++ src/main/java/tech/code0/communication/RabbitConnection.java
@@ -0,0 +1,42 @@
+package tech.code0.communication;
+
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import lombok.Getter;
+import tech.code0.configuration.AquilaConfiguration;
+
+import static tech.code0.AquilaServer.LOGGER;
+
+@Getter
+public class RabbitConnection {
+
+    private final Connection connection;
+
+    public RabbitConnection(AquilaConfiguration aquilaConfiguration) {
+        LOGGER.info("Initializing Rabbit connection");
+        this.connection = createConnection(aquilaConfiguration);
+    }
+
+    private Connection createConnection(AquilaConfiguration aquilaConfiguration) {
+        final var connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost(aquilaConfiguration.getRabbitMQHost());
+        connectionFactory.setPort(aquilaConfiguration.getRabbitMQPort());
+
+        try (final var connection = connectionFactory.newConnection()) {
+            LOGGER.info("Connected to RabbitMQ");
+            return connection;
+        } catch (Exception exception) {
+            LOGGER.warning("Connection to RabbitMQ failed: " + exception.getMessage());
+            throw new RuntimeException(exception);
+        }
+    }
+
+    public void close() {
+        try {
+            LOGGER.warning("Closing RabbitMQ connection");
+            this.connection.close();
+        } catch (Exception exception) {
+            LOGGER.severe("Failed to close RabbitMQ connection: " + exception.getMessage());
+        }
+    }
+}
