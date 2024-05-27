33da90b1f4a21ffd10609b9c49020cb8455702be
Raphael <raphael.goetz.7@gmail.com>
2024-05-27 20:29:41 +0200

    feat: added function to receive a flow from redis

--- src/main/java/tech/code0/data/RedisConnection.java
+++ src/main/java/tech/code0/data/RedisConnection.java
@@ -1,11 +1,16 @@
 package tech.code0.data;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
 import io.lettuce.core.RedisClient;
 import io.lettuce.core.api.StatefulRedisConnection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import lombok.Getter;
 import tech.code0.configuration.AquilaConfiguration;
+import tech.code0.grpc.FlowOuterClass;
+
+import java.util.Optional;
 
 public class RedisConnection {
 
@@ -32,6 +37,20 @@ public class RedisConnection {
         this.logger.info("Shutting down RedisConnection");
         this.connection.close();
         this.client.shutdown();
+    }
+
+    public Optional<FlowOuterClass.Flow> getFlow(String flowId) {
+        final var result = connection.sync().get(flowId);
+        if (result == null) return Optional.empty();
+
+        try {
+            final var currentFlow = new Gson().fromJson(result, FlowOuterClass.Flow.class);
+            return Optional.of(currentFlow);
+
+        } catch (JsonSyntaxException jsonSyntaxException) {
+            this.logger.warn(STR."Error parsing flow response with id: \{flowId}", jsonSyntaxException);
+            return Optional.empty();
+        }
     }
 
 }
