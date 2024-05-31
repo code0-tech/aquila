c7ef6557d30851df237a8373608d6f75ef09a220
Raphael <raphael.goetz.7@gmail.com>
2024-05-31 21:18:02 +0200

    fix: changed time to unix format

--- src/main/java/tech/code0/validation/ChecksumValidation.java
+++ src/main/java/tech/code0/validation/ChecksumValidation.java
@@ -3,17 +3,16 @@ package tech.code0.validation;
 import lombok.experimental.UtilityClass;
 import tech.code0.grpc.FlowOuterClass;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.time.Instant;
+import java.util.Date;
 
 @UtilityClass
 public class ChecksumValidation {
 
     public boolean hasEqualTime(FlowOuterClass.Flow usedFlow, FlowOuterClass.Flow accurateFlow) {
-        final var formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
-        final var usedTime = LocalDateTime.parse(usedFlow.getLastUpdated(), formatter);
-        final var accurateTime = LocalDateTime.parse(accurateFlow.getLastUpdated(), formatter);
-        return usedTime.isEqual(accurateTime);
+        final var usedTime = Date.from(Instant.ofEpochSecond(usedFlow.getLastUpdated()));
+        final var accurateTime = Date.from(Instant.ofEpochSecond(accurateFlow.getLastUpdated()));
+        return usedTime.equals(accurateTime);
     }
 
 }
