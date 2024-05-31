366c7da84cf8c00d48ee6a9cfebf7f19dd7638fa
Raphael <raphael.goetz.7@gmail.com>
2024-05-31 20:54:00 +0200

    fix: changed flow to flow-request

--- src/main/java/tech/code0/service/checksum/FlowCallback.java
+++ src/main/java/tech/code0/service/checksum/FlowCallback.java
@@ -5,31 +5,38 @@ import io.grpc.Status;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import tech.code0.data.RedisConnection;
+import tech.code0.data.flow.FlowService;
 import tech.code0.grpc.FlowOuterClass;
 import tech.code0.validation.ChecksumValidation;
 
-public class FlowCallback implements FutureCallback<FlowOuterClass.Flow> {
+public class FlowCallback implements FutureCallback<FlowOuterClass.FlowResponse> {
 
     private final Logger logger;
     private final RedisConnection connection;
-    public final FlowOuterClass.Flow currentFlow;
+    private final FlowService flowService;
 
-    public FlowCallback(FlowOuterClass.Flow currentFlow, RedisConnection redisConnection, Logger logger) {
+    public FlowCallback(FlowService flowService, RedisConnection redisConnection, Logger logger) {
         this.logger = logger;
-        this.currentFlow = currentFlow;
+        this.flowService = flowService;
         this.connection = redisConnection;
     }
 
     @Override
-    public void onSuccess(FlowOuterClass.Flow result) {
-        final var isOutdated = ChecksumValidation.hasEqualTime(currentFlow, result);
-        if (!isOutdated) return;
-        this.connection.getConnection().async().set(STR."flow:\{currentFlow.getFlowId()}", result.toString());
+    public void onSuccess(FlowOuterClass.FlowResponse result) {
+        result.getFlowsList().forEach(this::checkFlow);
     }
 
     @Override
     public void onFailure(@NotNull Throwable throwable) {
         final var status = Status.fromThrowable(throwable.getCause());
         this.logger.error(STR."Flow couldn't be recived\{status.getDescription()}", throwable);
+    }
+
+    private void checkFlow(FlowOuterClass.Flow reponseFlow) {
+        flowService.getFlow(reponseFlow.getFlowId()).thenAccept(currentFlow -> {
+            final var isOutdated = ChecksumValidation.hasEqualTime(currentFlow, reponseFlow);
+            if (!isOutdated) return;
+            this.connection.getConnection().async().set(STR."flow:\{currentFlow.getFlowId()}", reponseFlow.toString());
+        });
     }
 }
