35230460e3e2f970474d71be385865fda65ab61d
Raphael <raphael.goetz.7@gmail.com>
2024-06-03 20:36:59 +0200

    dependencies: switched to micronaut

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@ import com.google.protobuf.gradle.id
 
 plugins {
     id("java")
+    id("io.micronaut.application") version "4.4.0"
+    id("io.micronaut.test-resources") version "4.4.0"
     id("com.google.protobuf") version "0.9.4"
 }
 
@@ -13,24 +15,48 @@ repositories {
 }
 
 dependencies {
-    implementation("com.rabbitmq:amqp-client:5.21.0")
-    implementation("io.lettuce:lettuce-core:6.3.2.RELEASE")
+
+    annotationProcessor("io.micronaut.serde:micronaut-serde-processor")
+    implementation("io.micronaut:micronaut-discovery-core")
+    implementation("io.micronaut.grpc:micronaut-grpc-runtime")
+    implementation("io.micronaut.rabbitmq:micronaut-rabbitmq")
+    implementation("io.micronaut.redis:micronaut-redis-lettuce")
+    implementation("io.micronaut.serde:micronaut-serde-jackson")
+    implementation("javax.annotation:javax.annotation-api")
+    runtimeOnly("ch.qos.logback:logback-classic")
+
     implementation("com.gitlab.taucher2003.t2003-utils:log:1.1-beta.13")
     compileOnly("org.projectlombok:lombok:1.18.32")
     annotationProcessor("org.projectlombok:lombok:1.18.32")
 
-    implementation("io.grpc:grpc-netty-shaded:1.64.0")
-    implementation("io.grpc:grpc-protobuf:1.64.0")
-    implementation("io.grpc:grpc-stub:1.64.0")
-
-    implementation("com.google.code.gson:gson:2.11.0")
-    implementation("com.google.protobuf:protobuf-java:4.27.0")
-
-    implementation("javax.annotation:javax.annotation-api:1.3.2")
 }
 
-java.toolchain {
-    languageVersion = JavaLanguageVersion.of(21)
+application {
+    mainClass = "tech.code0.AquilaApplication"
+}
+java {
+    sourceCompatibility = JavaVersion.toVersion("22")
+    targetCompatibility = JavaVersion.toVersion("22")
+}
+
+sourceSets {
+    main {
+        java {
+            srcDirs("build/generated/source/proto/main/grpc")
+            srcDirs("build/generated/source/proto/main/java")
+        }
+    }
+}
+
+micronaut {
+    testRuntime("junit5")
+    processing {
+        incremental(true)
+        annotations("tech.code0.*")
+    }
+    testResources {
+        sharedServer = true
+    }
 }
 
 protobuf {
@@ -55,4 +81,8 @@ protobuf {
 
 tasks.withType<JavaCompile> {
     options.compilerArgs.add("--enable-preview")
+}
+
+tasks.named<io.micronaut.gradle.docker.NativeImageDockerfile>("dockerfileNative") {
+    jdkVersion = "22"
 }
