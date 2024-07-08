92a28a6949208775ee03b856a78488c7111688d0
Raphael <raphael.goetz.7@gmail.com>
2024-07-08 20:56:18 +0200

    feat: added flow service tests

--- src/test/java/tech/code0/service/FlowServiceTest.java
+++ src/test/java/tech/code0/service/FlowServiceTest.java
@@ -1,16 +1,19 @@
 package tech.code0.service;
 
+import io.lettuce.core.RedisFuture;
 import io.lettuce.core.api.StatefulRedisConnection;
 import io.lettuce.core.api.async.RedisAsyncCommands;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import tech.code0.model.FlowOuterClass;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
@@ -26,7 +29,7 @@ public class FlowServiceTest {
     @Mock
     private RedisAsyncCommands<String, String> commands;
 
-    @InjectMocks
+    @Inject
     private FlowService flowService;
 
     @BeforeEach
@@ -36,13 +39,82 @@ public class FlowServiceTest {
     }
 
     @Test
-    void testUpdateFlow() throws Exception {
+    void testUpdateFlowTrue() throws Exception {
         FlowOuterClass.Flow flow = FlowOuterClass.Flow.newBuilder().setFlowId(1).build();
-        when(commands.set(eq("1:1"), any()));
+
+        RedisFuture<String> redisFuture = mock(RedisFuture.class);
+        when(commands.set(eq("1:1"), any())).thenReturn(redisFuture);
+        when(redisFuture.get()).thenReturn("OK");
 
         CompletableFuture<Boolean> result = flowService.updateFlow(1L, flow);
-
-        assertTrue(result.get());
-        verify(commands, times(1)).set(eq("1:1"), any());
+        assertTrue(result.join());
     }
+
+    @Test
+    void testUpdateFlows_Success() throws ExecutionException, InterruptedException {
+        // Arrange
+        FlowOuterClass.Flow flow1 = FlowOuterClass.Flow.newBuilder().setFlowId(1).build();
+        FlowOuterClass.Flow flow2 = FlowOuterClass.Flow.newBuilder().setFlowId(2).build();
+        List<FlowOuterClass.Flow> flows = List.of(flow1, flow2);
+        RedisFuture<String> redisFuture = mock(RedisFuture.class);
+
+        when(commands.set(eq("1:1"), any())).thenReturn(redisFuture);
+        when(commands.set(eq("1:2"), any())).thenReturn(redisFuture);
+        when(redisFuture.get()).thenReturn("OK");
+
+        CompletableFuture<Boolean> result = flowService.updateFlows(1L, flows);
+        assertTrue(result.join());
+    }
+
+    @Test
+    void testDeleteFlow_Success() throws Exception {
+        RedisFuture<Long> redisFuture = mock(RedisFuture.class);
+        when(commands.del(eq("1:1"))).thenReturn(redisFuture);
+        when(redisFuture.get()).thenReturn(1L);
+
+        CompletableFuture<Boolean> result = flowService.deleteFlow(1L, 1L);
+        assertTrue(result.join());
+    }
+
+    @Test
+    void testDeleteFlows_Success() throws Exception {
+        FlowOuterClass.Flow flow1 = FlowOuterClass.Flow.newBuilder().setFlowId(1).build();
+        FlowOuterClass.Flow flow2 = FlowOuterClass.Flow.newBuilder().setFlowId(2).build();
+        List<FlowOuterClass.Flow> flows = List.of(flow1, flow2);
+
+        RedisFuture<Long> redisFuture = mock(RedisFuture.class);
+
+        when(commands.del(any(String[].class))).thenReturn(redisFuture);
+        when(redisFuture.get()).thenReturn(2L);
+
+        CompletableFuture<Boolean> result = flowService.deleteFlows(1L, flows);
+        assertTrue(result.join());
+    }
+
+    @Test
+    void testOverWrite_Success() throws ExecutionException, InterruptedException {
+        FlowOuterClass.Flow flow1 = FlowOuterClass.Flow.newBuilder().setFlowId(1).build();
+        FlowOuterClass.Flow flow2 = FlowOuterClass.Flow.newBuilder().setFlowId(2).build();
+        List<FlowOuterClass.Flow> flows = List.of(flow1, flow2);
+
+        RedisFuture<List<String>> redisKeysFuture = mock(RedisFuture.class);
+        RedisFuture<Long> redisDelFuture = mock(RedisFuture.class);
+
+        when(commands.keys(any())).thenReturn(redisKeysFuture);
+        when(redisKeysFuture.get()).thenReturn(List.of("1:1", "1:2"));
+        when(commands.del(any(String[].class))).thenReturn(redisDelFuture);
+        when(redisDelFuture.get()).thenReturn(2L);
+
+        CompletableFuture<Boolean> result = flowService.updateFlows(1L, flows);
+        assertTrue(result.join());
+    }
+
+    @Test
+    void testUpdateFlows_EmptyList() {
+        List<FlowOuterClass.Flow> emptyList = List.of();
+        CompletableFuture<Boolean> result = flowService.updateFlows(1L, emptyList);
+
+        assertTrue(result.join());
+    }
+
 }
