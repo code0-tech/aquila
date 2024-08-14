82e6ad48a072196d32d968c424ab99257d26f01c
Raphael <raphael.goetz.7@gmail.com>
2024-08-14 09:01:44 +0200

    feat: created start configuration struct to hold all configuration logic

--- src/configuration/start_configuration.rs
+++ src/configuration/start_configuration.rs
@@ -6,51 +6,63 @@ use clokwerk::AsyncScheduler;
 use log::{error, info};
 use redis::aio::MultiplexedConnection;
 use tokio::sync::Mutex;
-use tonic::transport::Server;
+use tonic::transport::{Channel, Server};
+use tucana_internal::internal::Flow;
 use tucana_internal::internal::flow_aquila_service_server::FlowAquilaServiceServer;
 use tucana_internal::internal::flow_sagittarius_service_client::FlowSagittariusServiceClient;
 use crate::client::flow_client::FlowClient;
 use crate::endpoint::flow_endpoint::FlowEndpoint;
 
-pub async fn init_endpoints(connection_arc: Arc<Mutex<Box<MultiplexedConnection>>>) {
-    let has_grpc_enabled = get_env_with_default("ENABLE_GRPC_UPDATE", false);
-
-    if !has_grpc_enabled {
-        return;
-    }
-
-    let addr = "[::1]:50051".parse().unwrap();
-    let service = FlowEndpoint::new(connection_arc);
-
-    let server = Server::builder()
-        .add_service(FlowAquilaServiceServer::new(service))
-        .serve(addr).await;
-
-    match server {
-        Ok(_) => info!("Started Flow-Endpoint"),
-        Err(server_error) => error!("Can't start Flow-Endpoint {server_error}")
-    }
+pub struct StartConfiguration {
+    connection_arc: Arc<Mutex<Box<MultiplexedConnection>>>,
+    flow_client: FlowClient
 }
 
-pub async fn init_client(connection_arc: Arc<Mutex<Box<MultiplexedConnection>>>) {
-    let client = match FlowSagittariusServiceClient::connect("https://[::1]:50051").await {
-        Ok(res) => res,
-        Err(start_error) => {
-            error!("Can't start client {start_error}");
-            return;
-        }
-    };
+impl StartConfiguration {
 
-    let mut flow_client = FlowClient::new(connection_arc, client).await;
-    let has_scheduled_enabled = get_env_with_default("ENABLE_SCHEDULED_UPDATE", false);
+    pub async fn new(connection_arc: Arc<Mutex<Box<MultiplexedConnection>>>) -> Self {
+        let client = match FlowSagittariusServiceClient::connect("https://[::1]:50051").await {
+            Ok(res) => res,
+            Err(start_error) => {
+                panic!("Can't start client {start_error}");
+            }
+        };
 
-    if !has_scheduled_enabled {
-        flow_client.send_get_flow_request().await;
-        return;
+        let flow_client = FlowClient::new(connection_arc.clone(), client).await;  
+        Self { connection_arc, flow_client }
     }
 
-    let schedule_interval = get_env_with_default("UPDATE_SCHEDULE_INTERVAL", 0);
-    let mut scheduler = AsyncScheduler::new();
+
+    pub async fn init_endpoints(&self, connection_arc: Arc<Mutex<Box<MultiplexedConnection>>>) {
+        let has_grpc_enabled = get_env_with_default("ENABLE_GRPC_UPDATE", false);
+
+        if !has_grpc_enabled {
+            return;
+        }
+
+        let addr = "[::1]:50051".parse().unwrap();
+        let service = FlowEndpoint::new(connection_arc);
+
+        let server = Server::builder()
+            .add_service(FlowAquilaServiceServer::new(service))
+            .serve(addr).await;
+
+        match server {
+            Ok(_) => info!("Started Flow-Endpoint"),
+            Err(server_error) => error!("Can't start Flow-Endpoint {server_error}")
+        }
+    }
+
+    pub async fn init_client(&mut self) {
+        let has_scheduled_enabled = get_env_with_default("ENABLE_SCHEDULED_UPDATE", false);
+
+        if !has_scheduled_enabled {
+            self.flow_client.send_get_flow_request().await;
+            return;
+        }
+
+        let schedule_interval = get_env_with_default("UPDATE_SCHEDULE_INTERVAL", 0);
+        let mut scheduler = AsyncScheduler::new();
 
         todo!("Work on the shit below");
         /*
