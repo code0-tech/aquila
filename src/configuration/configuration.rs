cfcdb884ace8967385fb2677a57f3c37af078d8a
Raphael <raphael.goetz.7@gmail.com>
2024-08-14 08:37:02 +0200

    ref: put function for getting env. and parse them into its own generic function

--- src/configuration/configuration.rs
+++ src/configuration/configuration.rs
@@ -1,3 +1,6 @@
+use std::fs::File;
+use std::io::Read;
+use std::str::FromStr;
 use std::sync::Arc;
 use clokwerk::AsyncScheduler;
 use log::{error, info};
@@ -10,21 +13,7 @@ use crate::client::flow_client::FlowClient;
 use crate::endpoint::flow_endpoint::FlowEndpoint;
 
 pub async fn init_endpoints(connection_arc: Arc<Mutex<Box<MultiplexedConnection>>>) {
-    let has_grpc_enabled_env = match std::env::var("ENABLE_GRPC_UPDATE") {
-        Ok(env) => env,
-        Err(var_error) => {
-            error!("Env. Variable ENABLE_GRPC_UPDATE wasn't found. Reason: {var_error}");
-            return;
-        }
-    };
-
-    let has_grpc_enabled: bool = match has_grpc_enabled_env.parse() {
-        Ok(env) => env,
-        Err(parse_error) => {
-            error!("Can't parse variable. Reason: {parse_error}");
-            return;
-        }
-    };
+    let has_grpc_enabled = get_env_with_default("ENABLE_GRPC_UPDATE", false);
 
     if !has_grpc_enabled {
         return;
@@ -44,7 +33,6 @@ pub async fn init_endpoints(connection_arc: Arc<Mutex<Box<MultiplexedConnection>
 }
 
 pub async fn init_client(connection_arc: Arc<Mutex<Box<MultiplexedConnection>>>) {
-
     let client = match FlowSagittariusServiceClient::connect("https://[::1]:50051").await {
         Ok(res) => res,
         Err(start_error) => {
@@ -54,44 +42,14 @@ pub async fn init_client(connection_arc: Arc<Mutex<Box<MultiplexedConnection>>>)
     };
 
     let mut flow_client = FlowClient::new(connection_arc, client).await;
-
-    let has_scheduled_enabled_env = match std::env::var("ENABLE_SCHEDULED_UPDATE") {
-        Ok(env) => env,
-        Err(var_error) => {
-            error!("Env. Variable ENABLE_SCHEDULED_UPDATE wasn't found. Reason: {var_error}");
-            return;
-        }
-    };
-
-    let has_scheduled_enabled: bool = match has_scheduled_enabled_env.parse() {
-        Ok(env) => env,
-        Err(parse_error) => {
-            error!("Can't parse variable. Reason: {parse_error}");
-            return;
-        }
-    };
+    let has_scheduled_enabled = get_env_with_default("ENABLE_SCHEDULED_UPDATE", false);
 
     if !has_scheduled_enabled {
         flow_client.send_get_flow_request().await;
         return;
     }
 
-    let schedule_interval_env = match std::env::var("UPDATE_SCHEDULE_INTERVAL") {
-        Ok(interval_env) => interval_env,
-        Err(err) => {
-            error!("ENABLE_SCHEDULED_UPDATE true but UPDATE_SCHEDULE_INTERVAL not set: {err}");
-            return;
-        }
-    };
-
-    let schedule_interval = match u32::from_str(&schedule_interval_env) {
-        Ok(interval) => interval,
-        Err(err) => {
-            error!("Cannot parse UPDATE_SCHEDULE_INTERVAL to u32: {err}");
-            return;
-        }
-    };
-
+    let schedule_interval = get_env_with_default("UPDATE_SCHEDULE_INTERVAL", 0);
     let mut scheduler = AsyncScheduler::new();
 
     todo!("Work on the shit below");
