86dda3fe18ec7dd3cd6c7d6d6e89624ccfdcd943
Raphael <raphael.goetz.7@gmail.com>
2024-10-31 15:31:59 +0100

    feat: added debug messages to redis client

--- src/service/flow_service.rs
+++ src/service/flow_service.rs
@@ -1,6 +1,7 @@
 use std::sync::{Arc};
 use async_trait::async_trait;
-use log::error;
+use futures::future::err;
+use log::{debug, error};
 use redis::aio::MultiplexedConnection;
 use redis::{AsyncCommands, RedisError};
 use tokio::sync::Mutex;
@@ -30,7 +31,16 @@ impl FlowService for FlowServiceBase {
     async fn insert_flow(&mut self, flow: Flow) {
         let mut connection = self.redis_client_arc.lock().await;
         let serialized_flow = serde_json::to_string(&flow).expect("");
-        connection.set::<String, String, ()>(flow.flow_id.to_string(), serialized_flow).await.expect("Aga");
+        let parsed_flow = connection.set::<String, String, ()>(flow.flow_id.to_string(), serialized_flow).await;
+
+        match parsed_flow {
+            Ok(_) => {
+                debug!("Inserted flow");
+            },
+            Err(redis_error) => {
+                error!("An Error occurred {}", redis_error);
+            }
+        }
     }
 
     async fn insert_flows(&mut self, flows: Vec<Flow>) {
@@ -39,13 +49,22 @@ impl FlowService for FlowServiceBase {
         for flow in flows {
             let serialized_flow = serde_json::to_string(&flow);
 
-            match serialized_flow {
+             let parsed_flow = match serialized_flow {
                 Ok(parsed_flow) => {
-                    connection.set::<String, String, i64>(flow.flow_id.to_string(), parsed_flow);
+                   connection.set::<String, String, i64>(flow.flow_id.to_string(), parsed_flow).await
                 }
                 Err(parse_error) => {
                     error!("Can't parse {} Because: {}", flow.flow_id, parse_error);
                     continue
+                }
+            };
+            
+            match parsed_flow { 
+                Ok(_) => {
+                    debug!("Inserted flow");
+                },
+                Err(redis_error) => {
+                    error!("An Error occurred {}", redis_error);
                 }
             }
         }
@@ -53,12 +72,30 @@ impl FlowService for FlowServiceBase {
 
     async fn delete_flow(&mut self, flow_id: i64) {
         let mut connection = self.redis_client_arc.lock().await;
-        connection.del::<i64, i64>(flow_id);
+        let deleted_flow = connection.del::<i64, i64>(flow_id).await;
+
+        match deleted_flow {
+            Ok(changed_amount) => {
+                debug!("{} flows where deleted", changed_amount);
+            },
+            Err(redis_error) => {
+                error!("An Error occurred {}", redis_error);
+            }
+        }
     }
 
     async fn delete_flows(&mut self, flow_ids: Vec<i64>) {
         let mut connection = self.redis_client_arc.lock().await;
-        connection.del::<Vec<i64>, i64>(flow_ids);
+        let deleted_flow = connection.del::<Vec<i64>, i64>(flow_ids).await;
+
+        match deleted_flow {
+            Ok(changed_amount) => {
+                debug!("{} flows where deleted", changed_amount);
+            },
+            Err(redis_error) => {
+                error!("An Error occurred {}", redis_error);
+            }
+        }
     }
 
     
