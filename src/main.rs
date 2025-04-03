43d7bc581e0206b509f4b45f1f4d1493708d2000
Raphael <raphael.goetz.7@gmail.com>
2025-04-03 17:14:34 +0200

    feat: added static startup mode

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-use std::sync::Arc;
-
 use code0_flow::flow_store::{
     connection::create_flow_store_connection,
     service::{FlowStoreService, FlowStoreServiceBase},
 };
 use configuration::config::Config;
 use sagittarius::flow_service_client_impl::SagittariusFlowClient;
+use serde_json::from_str;
+use std::{fs::File, io::Read, sync::Arc};
 use tokio::sync::Mutex;
+use tucana::sagittarius::Flows;
 
 pub mod authorization;
 pub mod configuration;
@@ -28,5 +29,40 @@ async fn main() {
                 .await;
 
         sagittarius_client.init_flow_stream().await;
+    } else {
+        init_flows_from_json(config.flow_fallback_path, flow_store_client).await
     }
+}
+
+async fn init_flows_from_json(path: String, flow_store_client: Arc<Mutex<FlowStoreService>>) {
+    let mut data = String::new();
+
+    let mut file = match File::open(path) {
+        Ok(file) => file,
+        Err(error) => {
+            panic!("There was a problem opening the file: {:?}", error);
+        }
+    };
+
+    match file.read_to_string(&mut data) {
+        Ok(_) => {
+            print!("Successfully read data from file");
+        }
+        Err(error) => {
+            panic!("There was a problem reading the file: {:?}", error);
+        }
+    }
+
+    let flows: Flows = match from_str(&data) {
+        Ok(flows) => flows,
+        Err(error) => {
+            panic!(
+                "There was a problem deserializing the json file: {:?}",
+                error
+            );
+        }
+    };
+
+    let mut store = flow_store_client.lock().await;
+    let _ = store.insert_flows(flows).await;
 }
