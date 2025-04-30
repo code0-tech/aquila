e8ee275747ab97be6f55ab9570ae26dbc6e8cfca
Raphael <raphael.goetz.7@gmail.com>
2025-04-30 17:45:25 +0200

    feat: added runtime function service client & server

---
+++ src/sagittarius/runtime_function_service_client_impl.rs
@@ -0,0 +1,52 @@
+use crate::authorization::authorization::get_authorization_metadata;
+use std::sync::Arc;
+use tokio::sync::Mutex;
+use tonic::transport::Channel;
+use tonic::Extensions;
+use tonic::Request;
+use tucana::aquila::RuntimeFunctionDefinitionUpdateRequest as AquilaRuntimeFunctionUpdateRequest;
+use tucana::aquila::RuntimeFunctionDefinitionUpdateResponse as AquilaRuntimeFunctionUpdateResponse;
+use tucana::sagittarius::runtime_function_definition_service_client::RuntimeFunctionDefinitionServiceClient;
+use tucana::sagittarius::RuntimeFunctionDefinitionUpdateRequest as SagittariusRuntimeFunctionUpdateRequest;
+
+pub struct SagittariusRuntimeFunctionServiceClient {
+    client: RuntimeFunctionDefinitionServiceClient<Channel>,
+    token: String,
+}
+
+impl SagittariusRuntimeFunctionServiceClient {
+    pub async fn new(sagittarius_url: String, token: String) -> Self {
+        let client = match RuntimeFunctionDefinitionServiceClient::connect(sagittarius_url).await {
+            Ok(client) => client,
+            Err(e) => panic!("Failed to connect to Sagittarius: {}", e),
+        };
+
+        Self { client, token }
+    }
+
+    pub async fn new_arc(sagittarius_url: String, token: String) -> Arc<Mutex<Self>> {
+        Arc::new(Mutex::new(Self::new(sagittarius_url, token).await))
+    }
+
+    pub async fn update_runtime_function_definitions(
+        &mut self,
+        runtime_function_update_request: AquilaRuntimeFunctionUpdateRequest,
+    ) -> AquilaRuntimeFunctionUpdateResponse {
+        let request = Request::from_parts(
+            get_authorization_metadata(&self.token),
+            Extensions::new(),
+            SagittariusRuntimeFunctionUpdateRequest {
+                runtime_functions: runtime_function_update_request.runtime_functions,
+            },
+        );
+
+        let response = match self.client.update(request).await {
+            Ok(response) => response,
+            Err(_) => return AquilaRuntimeFunctionUpdateResponse { success: false },
+        };
+
+        AquilaRuntimeFunctionUpdateResponse {
+            success: response.into_inner().success,
+        }
+    }
+}
---
+++ src/server/runtime_function_service_server_impl.rs
@@ -0,0 +1,39 @@
+use std::sync::Arc;
+
+use tokio::sync::Mutex;
+use tucana::aquila::runtime_function_definition_service_server::RuntimeFunctionDefinitionService;
+
+use crate::sagittarius::runtime_function_service_client_impl::SagittariusRuntimeFunctionServiceClient;
+
+pub struct AquilaRuntimeFunctionServiceServer {
+    client: Arc<Mutex<SagittariusRuntimeFunctionServiceClient>>,
+}
+
+impl AquilaRuntimeFunctionServiceServer {
+    pub fn new(client: Arc<Mutex<SagittariusRuntimeFunctionServiceClient>>) -> Self {
+        Self { client }
+    }
+}
+
+#[tonic::async_trait]
+impl RuntimeFunctionDefinitionService for AquilaRuntimeFunctionServiceServer {
+    async fn update(
+        &self,
+        request: tonic::Request<tucana::aquila::RuntimeFunctionDefinitionUpdateRequest>,
+    ) -> std::result::Result<
+        tonic::Response<tucana::aquila::RuntimeFunctionDefinitionUpdateResponse>,
+        tonic::Status,
+    > {
+        let runtime_function_definition_update_request = request.into_inner();
+        let mut client = self.client.lock().await;
+        let response = client
+            .update_runtime_function_definitions(runtime_function_definition_update_request)
+            .await;
+
+        Ok(tonic::Response::new(
+            tucana::aquila::RuntimeFunctionDefinitionUpdateResponse {
+                success: response.success,
+            },
+        ))
+    }
+}
