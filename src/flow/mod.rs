3dab80795824a9860107138e93e5b8e0e6327ad5
raphael-goetz <raphael.goetz.7@gmail.com>
2025-12-19 20:57:46 +0100

    ref: match pattern with docs

--- src/flow/mod.rs
+++ src/flow/mod.rs
@@ -1,7 +1,7 @@
 use tucana::shared::ValidationFlow;
 
 /// Every flow identifier has this key
-/// type::project_slug::flow_id
+/// <type>.<project_slug>.<flow_id>
 pub fn get_flow_identifier(flow: &ValidationFlow) -> String {
     format!("{}.{}.{}", flow.r#type, flow.project_slug, flow.flow_id)
 }
