8f9cab37e7b0a9036ec6dc86caeddbd5e39ee7df
Raphael <raphael.goetz.7@gmail.com>
2026-02-26 13:23:16 +0100

    fix: added missing project_id to flow identifier

--- src/flow/mod.rs
+++ src/flow/mod.rs
@@ -3,5 +3,5 @@ use tucana::shared::ValidationFlow;
 /// Every flow identifier has this key
 /// <type>.<project_slug>.<flow_id>
 pub fn get_flow_identifier(flow: &ValidationFlow) -> String {
-    format!("{}.{}.{}", flow.r#type, flow.project_slug, flow.flow_id)
+    format!("{}.{}.{}.{}", flow.r#type, flow.project_slug, flow.project_id, flow.flow_id)
 }
