9d3045146f9f591afb2822f63cf3d650acb3a659
Raphael <raphael.goetz.7@gmail.com>
2026-04-30 15:46:13 +0200

    feat: finished implementation for test execution

--- src/sagittarius/test_execution_client_impl.rs
+++ src/sagittarius/test_execution_client_impl.rs
@@ -8,12 +8,14 @@ use futures::StreamExt;
 use prost::Message;
 use std::sync::Arc;
 use tokio_stream::wrappers::ReceiverStream;
-use tonic::Request;
 use tonic::transport::Channel;
+use tonic::{Extensions, Request};
 use tucana::sagittarius::execution_logon_request::Data;
 use tucana::sagittarius::execution_service_client::ExecutionServiceClient;
 use tucana::sagittarius::{ExecutionLogonRequest, Logon};
 use tucana::shared::{ExecutionFlow, ExecutionResult, ValidationFlow};
+
+use crate::authorization::authorization::get_authorization_metadata;
 
 pub struct SagittariusTestExecutionServiceClient {
     nats_client: async_nats::Client,
@@ -23,23 +25,13 @@ pub struct SagittariusTestExecutionServiceClient {
 }
 
 impl SagittariusTestExecutionServiceClient {
-    pub async fn new(
+    pub fn new(
         nats_client: async_nats::Client,
         store: Arc<async_nats::jetstream::kv::Store>,
-        sagittarius_url: String,
+        channel: Channel,
         token: String,
     ) -> Self {
-        let client = match ExecutionServiceClient::connect(sagittarius_url).await {
-            Ok(client) => {
-                log::info!("Successfully connected to Sagittarius RuntimeFunction Endpoint!");
-                client
-            }
-            Err(err) => panic!(
-                "Failed to connect to Sagittarius (RuntimeFunction Endpoint): {:?}",
-                err
-            ),
-        };
-
+        let client = ExecutionServiceClient::new(channel);
         Self {
             nats_client,
             store,
@@ -74,8 +66,13 @@ impl SagittariusTestExecutionServiceClient {
         let logon = ExecutionLogonRequest {
             data: Some(Data::Logon(Logon {})),
         };
+        let request = Request::from_parts(
+            get_authorization_metadata(&self.token),
+            Extensions::new(),
+            ack,
+        );
 
-        let mut test_execution_stream = match self.client.test(Request::new(ack)).await {
+        let mut test_execution_stream = match self.client.test(request).await {
             Ok(response) => response.into_inner(),
             Err(error) => {
                 log::error!("Received status code: {:?}", error);
@@ -83,53 +80,90 @@ impl SagittariusTestExecutionServiceClient {
             }
         };
 
-        tx.send(logon).await.unwrap();
+        if let Err(err) = tx.send(logon).await {
+            log::error!("Failed to send test execution logon: {:?}", err);
+            match test_execution_stream.message().await {
+                Ok(Some(_)) => {
+                    log::warn!("Test execution stream produced data even though logon send failed");
+                }
+                Ok(None) => {
+                    log::warn!("Test execution stream closed before logon could be sent");
+                }
+                Err(status) => {
+                    log::error!(
+                        "Test execution stream closed with status code={} message={}",
+                        status.code(),
+                        status.message()
+                    );
+                }
+            }
+            return;
+        }
 
-        while let Some(Ok(test_execution_request)) = test_execution_stream.next().await {
-            if let Some(request) = test_execution_request.request {
-                let validation_flow = match self.load_validation_flow(request.flow_id).await {
-                    Some(flow) => flow,
-                    None => {
-                        continue;
-                    }
-                };
+        while let Some(next) = test_execution_stream.next().await {
+            match next {
+                Ok(test_execution_request) => {
+                    if let Some(request) = test_execution_request.request {
+                        let validation_flow = match self.load_validation_flow(request.flow_id).await
+                        {
+                            Some(flow) => flow,
+                            None => {
+                                continue;
+                            }
+                        };
 
-                let uuid = uuid::Uuid::new_v4().to_string();
+                        let uuid = uuid::Uuid::new_v4().to_string();
 
-                // TODO: When the new validator is ready, the body needs to be validated at this
-                // point.
+                        // TODO: When the new validator is ready, the body needs to be validated at this
+                        // point.
 
-                let execution_flow = ExecutionFlow {
-                    flow_id: request.flow_id,
-                    input_value: request.body,
-                    starting_node_id: validation_flow.starting_node_id,
-                    node_functions: validation_flow.node_functions,
-                    project_id: validation_flow.project_id,
-                };
+                        let execution_flow = ExecutionFlow {
+                            flow_id: request.flow_id,
+                            input_value: request.body,
+                            starting_node_id: validation_flow.starting_node_id,
+                            node_functions: validation_flow.node_functions,
+                            project_id: validation_flow.project_id,
+                        };
 
-                let bytes = execution_flow.encode_to_vec();
-                let topic = format!("test_execution.{}", uuid);
-                let result = self.nats_client.request(topic, bytes.into()).await;
+                        let bytes = execution_flow.encode_to_vec();
+                        let topic = format!("test_execution.{}", uuid);
+                        let result = self.nats_client.request(topic, bytes.into()).await;
 
-                // Aquila will expect a `Execution Result` back from Taurus
-                match result {
-                    Ok(message) => match ExecutionResult::decode(message.payload) {
-                        Ok(value) => {
-                            let execution_result = ExecutionLogonRequest {
-                                data: Some(Data::Response(value)),
-                            };
+                        // Aquila will expect a `Execution Result` back from Taurus
+                        match result {
+                            Ok(message) => match ExecutionResult::decode(message.payload) {
+                                Ok(value) => {
+                                    let execution_result = ExecutionLogonRequest {
+                                        data: Some(Data::Response(value)),
+                                    };
 
-                            if let Err(err) = tx.send(execution_result).await {
-                                log::error!("Failed to send ExecutionLogonResponse: {:?}", err);
+                                    if let Err(err) = tx.send(execution_result).await {
+                                        log::error!(
+                                            "Failed to send ExecutionLogonResponse: {:?}",
+                                            err
+                                        );
+                                    }
+                                }
+                                Err(err) => {
+                                    log::error!(
+                                        "Failed to decode response from NATS server: {:?}",
+                                        err
+                                    );
+                                }
+                            },
+                            Err(err) => {
+                                log::error!("Failed to send request to NATS server: {:?}", err);
                             }
                         }
-                        Err(err) => {
-                            log::error!("Failed to decode response from NATS server: {:?}", err);
-                        }
-                    },
-                    Err(err) => {
-                        log::error!("Failed to send request to NATS server: {:?}", err);
                     }
+                }
+                Err(status) => {
+                    log::error!(
+                        "Test execution stream error code={} message={}",
+                        status.code(),
+                        status.message()
+                    );
+                    break;
                 }
             }
         }
